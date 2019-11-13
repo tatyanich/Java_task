acbf6b87289c2647b71a4489b1a357d0364f3b86
tmatiunina <tmatiunina@foundationmedicine.com>
2019-11-13 17:36:24 -0500

    Add c# proj

---
+++ NunitTestExample/NunitTestExample/Class1.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitTestExample
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void FirstTest()
+        {
+            Assert.AreEqual(1,1);
+        }
+        [Test]
+        public void SecondtTest()
+        {
+            Assert.AreEqual(1,2);
+        }
+
+        [Test]
+        [Ignore("dddd")]
+        public void ThirdTest()
+        {
+            Assert.AreEqual(1,1);
+        }
+
+        [Test]
+        public void ForthTest()
+        {
+            Assert.AreEqual(1,1);
+        }
+    }
+}
